9cdc0d5274318317654f6c2113d39f46c4ca8ceb
Dazmi <36841434+Dazmi@users.noreply.github.com>
2019-09-04 19:15:50 +1000

    new shelled app

---
+++ League_of_players/League_of_players/League_of_players.MobileAppService/Models/IItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace League_of_players.Models
+{
+    public interface IItemRepository
+    {
+        void Add(Item item);
+        void Update(Item item);
+        Item Remove(string key);
+        Item Get(string id);
+        IEnumerable<Item> GetAll();
+    }
+}
