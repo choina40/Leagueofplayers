57f0aa059d7fc9395c6c199b759f78aea091f497
Dazmi <36841434+Dazmi@users.noreply.github.com>
2019-09-18 19:19:38 +1000

    compile working

---
+++ League_of_players/League_of_players/League_of_players/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace League_of_players.ViewModels
+{
+    public class ProfileViewModel : BaseViewModel
+    {
+        public ICommand GoCommand { get; set; }
+
+        public ProfileViewModel()
+        {
+            GoCommand = new Command(go);
+        }
+
+        private void go()
+        {
+            //Navigate code. 
+            //NavigationService.NavigateTo<NextViewModel>();
+        }
+    }
+}
