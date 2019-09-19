48acb5c87cbdbcb0b1c2d73d2313a6953c7eb325
unknown <36841434+Dazmi@users.noreply.github.com>
2019-09-19 15:48:26 +1000

    stable fix

--- League_of_players/League_of_players/League_of_players/ViewModels/HomeViewModel.cs
+++ League_of_players/League_of_players/League_of_players/ViewModels/HomeViewModel.cs
@@ -8,18 +8,19 @@ namespace League_of_players.ViewModels
 {
     class HomeViewModel : BaseViewModel
     {
-        public ICommand GoCommand { get; set; }
+        public ICommand GoCommand { set; get; }
 
         public HomeViewModel()
         {
-            Title = "About";
+            Title = "New";
             GoCommand = new Command(go);
+            
         }
+
         private void go()
         {
-            //Navigate code. 
-            //NavigationService.NavigateTo<NextViewModel>();
             //NavigationService.NavigateToAsync<AboutViewModel>();
         }
+        
     }
 }
