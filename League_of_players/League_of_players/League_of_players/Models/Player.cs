2ee6a2c53abda122bf65be39606d04b7db1ce0d0
Dazmi <36841434+Dazmi@users.noreply.github.com>
2019-09-25 16:36:26 +1000

    updated model and mock data

--- League_of_players/League_of_players/League_of_players/Models/Player.cs
+++ League_of_players/League_of_players/League_of_players/Models/Player.cs
@@ -4,8 +4,11 @@ namespace League_of_players.Models
 {
     public class Player
     {
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Bio { get; set; }
-        public string Description { get; set; }
+        public string Level { get; set; }
+        public string Rank { get; set; }
+        public string LP { get; set; }
     }
 }
--- League_of_players/League_of_players/League_of_players/Models/Team.cs
+++ League_of_players/League_of_players/League_of_players/Models/Team.cs
@@ -4,6 +4,7 @@ namespace League_of_players.Models
 {
     public class Team
     {
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Mode { get; set; }
