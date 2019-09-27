ea228e7000bf7a259c6fce7d5ec09a7ae2bac142
choina40 <54886233+choina40@users.noreply.github.com>
2019-09-28 09:53:43 +1000

    buttons

--- League_of_players/League_of_players/League_of_players.Android/Resources/Resource.designer.cs
+++ League_of_players/League_of_players/League_of_players.Android/Resources/Resource.designer.cs
@@ -6460,28 +6460,31 @@ namespace League_of_players.Droid
 			public const int notify_panel_notification_icon_bg = 2131165503;
 			
 			// aapt resource value: 0x7F070140
-			public const int summonericon = 2131165504;
+			public const int power = 2131165504;
 			
 			// aapt resource value: 0x7F070141
-			public const int summonericon3 = 2131165505;
+			public const int summonericon = 2131165505;
 			
 			// aapt resource value: 0x7F070142
-			public const int tab_about = 2131165506;
+			public const int summonericon3 = 2131165506;
 			
 			// aapt resource value: 0x7F070143
-			public const int tab_feed = 2131165507;
+			public const int tab_about = 2131165507;
 			
 			// aapt resource value: 0x7F070144
-			public const int tooltip_frame_dark = 2131165508;
+			public const int tab_feed = 2131165508;
 			
 			// aapt resource value: 0x7F070145
-			public const int tooltip_frame_light = 2131165509;
+			public const int tooltip_frame_dark = 2131165509;
 			
 			// aapt resource value: 0x7F070146
-			public const int xamarin_logo = 2131165510;
+			public const int tooltip_frame_light = 2131165510;
 			
 			// aapt resource value: 0x7F070147
-			public const int zed2 = 2131165511;
+			public const int xamarin_logo = 2131165511;
+			
+			// aapt resource value: 0x7F070148
+			public const int zed2 = 2131165512;
 			
 			static Drawable()
 			{
