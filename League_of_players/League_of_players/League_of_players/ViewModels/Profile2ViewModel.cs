1059e5977544ee3219c0ea259d07a76609da643c
choina40 <54886233+choina40@users.noreply.github.com>
2019-09-29 19:36:13 +1000

    Revert "deleted profile2"
    This reverts commit 2f265b23695a167ac3f4598e354ffe21f739cea2.

---
+++ League_of_players/League_of_players/League_of_players/ViewModels/Profile2ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace League_of_players.ViewModels
+{
+    public class Profile2ViewModel : BaseViewModel
+    {
+
+        public Profile2ViewModel()
+        {
+            Title = "Profile";
+
+            // vNavButtonCommand = new Command(async () => await NavigateTo(), () => !IsBusy);
+
+        }
+
+        string name = "Nagune";
+
+       
+
+        public string Name
+        {
+            get => name; //same as get { return name; }
+            set
+            {
+                SetProperty(ref name, value); //MvvmHelpers Implementation
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        string bio = "Bio: I am a noisy man who talk more instead of playing hard";
+        public string Bio
+        {
+            get => bio; //same as get { return name; }
+            set
+            {
+                SetProperty(ref bio, value); //MvvmHelpers Implementation
+                OnPropertyChanged(nameof(Bio));
+            }
+        }
+        // public Command NavButtonCommand { get; }
+        // async Task NavigateTo() => await Shell.Current.GoToAsync("///home");
+
+    }
+}
