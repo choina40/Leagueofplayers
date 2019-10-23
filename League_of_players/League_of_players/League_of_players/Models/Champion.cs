bbaacf31bee5850774755e54900cc96ad59b729e
Dazmi <36841434+Dazmi@users.noreply.github.com>
2019-10-23 18:35:00 +1000

    prac work

---
+++ League_of_players/League_of_players/League_of_players/Models/Champion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace League_of_players.Models
+{
+    public class Champion
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("profileIconId")]
+        public string ProfileIconId { get; set; }
+    }
+}
