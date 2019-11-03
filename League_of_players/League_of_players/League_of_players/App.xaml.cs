4dca7b18ada727211884927eb8b34e6290e43aad
RuiHongHeng <54886342+RuiHongHeng@users.noreply.github.com>
2019-11-03 16:37:43 +1000

    clean up code

--- League_of_players/League_of_players/League_of_players/App.xaml.cs
+++ League_of_players/League_of_players/League_of_players/App.xaml.cs
@@ -14,9 +14,6 @@ namespace League_of_players
 {
     public partial class App : Application
     {
-        //TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
-        //To debug on Android emulators run the web backend against .NET Core not IIS
-        //If using other emulators besides stock Google images you may need to adjust the IP address
         public static string AzureBackendUrl =
             DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000" : "http://localhost:5000";
         public static bool UseMockDataStore = true;
